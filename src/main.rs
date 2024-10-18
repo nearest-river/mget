cc441175a547b0b3eedc8520d76f9a4134d9d201
Nate River <trishiragolder69@outlook.com>
2024-10-18 23:59:37 +0530

    added target docs (optimized)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+
+mod target;
+use target::*;
+use clap::Parser;
+
+use reqwest::{
+  Url,
+  IntoUrl
+};
+
+
+#[derive(Parser)]
+struct App {
+  urls: Vec<Url>,
+  target_docs: Vec<DocType>
+}
+
+
+
+#[tokio::main]
+async fn main()-> anyhow::Result<()> {
+  let app=App::parse();
+  let urls=app.urls;
+  let pattern=app.target_docs.pattern();
+
+  for url in urls {
+    let path=url.path();
+    if pattern.is_match(path) {
+      download_vid(url).await?;
+      continue;
+    }
+    if !path.ends_with('/') {
+      log::warn!("invalid path: {path}\nSkipped..");
+      continue;
+    }
+
+
+    let html=reqwest::get(url.as_str()).await?.text().await?;
+    let _dom=tl::parse(&html,Default::default())?;
+    println!("for {url}:\n{html}");
+  }
+
+
+  Ok(())
+}
+
+
+async fn download_vid<T: IntoUrl>(url: T)-> anyhow::Result<()> {
+  let res=reqwest::get(url).await?;
+
+  println!("{res:#?}");
+  Ok(())
 }
